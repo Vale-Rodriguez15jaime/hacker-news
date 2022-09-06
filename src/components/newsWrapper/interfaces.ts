fa3eee55be3f104e2924f03f0e0fd5cbb48c68ec
Valeria RodriguezJ <valeria.rodriguez@simetrik.com>
2022-09-06 17:52:03 -0500

    🎨 Add interface newsInterface

--- src/components/newsWrapper/interfaces.ts
+++ src/components/newsWrapper/interfaces.ts
@@ -1,15 +1,24 @@
+import { initialStateTypes } from '../../actions/list/interfaces'
+
 export interface optionsInterface {
-    label: string
-    value: number
+  label: string
+  value: number
 }
 
 type StaticImageData = {
-    src: string,
-    blurDataURL: string,
-    height: number,
-    width: number
+  src: string
+  blurDataURL: string
+  height: number
+  width: number
 }
 
 export interface frameworkSelectedInterface {
-    value: string, label: string, flag: StaticImageData
+  value: string
+  label: string
+  flag: StaticImageData
+}
+
+export interface newsInterface {
+  get: Function
+  list: initialStateTypes
 }
