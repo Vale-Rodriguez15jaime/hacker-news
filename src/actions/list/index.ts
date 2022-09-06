6f0ac4a2398e7ed725ca0f63ff0433182c671994
Valeria RodriguezJ <valeria.rodriguez@simetrik.com>
2022-09-06 16:38:17 -0500

    ✨ Add file action and create function getList

---
+++ src/actions/list/index.ts
@@ -0,0 +1,28 @@
+import axios from "axios"
+import { Dispatch } from 'redux'
+
+import { listTypes } from './types'
+import { filtersType } from './interfaces'
+import { buildFilters } from '../../utils/tools'
+
+const BASE_URL = ' https://hn.algolia.com/api/v1/search_by_date'
+
+export const getList = (filters: filtersType[]) => {
+    return async (dispatch: Dispatch) => {
+        dispatch({
+            type: listTypes.GET
+        })
+        try {
+            const url = buildFilters(filters)
+            const response = await axios.get(`${BASE_URL}${url}`);
+            dispatch({
+                payload: { ...response },
+                type: listTypes.GET_SUCCESS
+            })
+        } catch (error) {
+            dispatch({
+                type: listTypes.GET_ERROR
+            })
+        }
+    }
+}
