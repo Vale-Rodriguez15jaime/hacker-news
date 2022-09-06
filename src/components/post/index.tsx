9d7c8fb7645d67b15add14c8c9de70e953b12022
Valeria RodriguezJ <valeria.rodriguez@simetrik.com>
2022-09-06 18:54:26 -0500

    🎨 Update component post to connect with real data

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,16 +1,21 @@
 import styles from './post.module.sass'
+import { itemNew } from '../../actions/list/interfaces'
+import moment from 'moment'
 
-const Post = () => {
+interface postInterface {
+  item: itemNew
+}
+
+const Post = ({ item }: postInterface) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.dataContainer}>
         <div className={styles.timeLabel}>
           {' '}
-          <i className="app-icon-time" /> 3 hours ago by author
+          <i className="app-icon-time" /> {moment(item.created_at).utc().fromNow()} by{' '}
+          {item.author}
         </div>
-        <div className={styles.message}>
-          Yes, React is taking over front-end development. The question is why
-        </div>
+        <div className={styles.message}>{item.story_title}</div>
       </div>
       <div className={styles.favContainer}>
         <i className="app-icon-heart-outline" />
