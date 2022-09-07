c3cdfecca989304790b7db1478bad2e037100842
Valeria RodriguezJ <valeria.rodriguez@simetrik.com>
2022-09-07 18:58:40 -0500

    🎨 Add new cases in reducers when is list fav

--- src/reducers/list.ts
+++ src/reducers/list.ts
@@ -5,6 +5,7 @@ import { initialStateTypes } from '../actions/list/interfaces'
 const initialState = {
   isLoading: false,
   hits: [],
+  favsList: [],
   hitsPerPage: 0,
   nbPages: 0,
   page: 0,
@@ -14,6 +15,7 @@ const initialState = {
 export default function (state: initialStateTypes = initialState, action: AnyAction) {
   switch (action.type) {
     case listTypes.GET:
+    case listTypes.GET_LIST_FAV:
       return {
         ...state,
         isLoading: true
@@ -24,7 +26,14 @@ export default function (state: initialStateTypes = initialState, action: AnyAct
         isLoading: false,
         ...action.payload
       }
+    case listTypes.GET_LIST_FAV_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        favsList: action.payload
+      }
     case listTypes.GET_ERROR:
+    case listTypes.GET_LIST_FAV_ERROR:
       return {
         ...state,
         isLoading: false
