252bde095bd0b8bbc773ec44dff2d95c4192d16b
Valeria RodriguezJ <valeria.rodriguez@simetrik.com>
2022-09-07 15:26:33 -0500

    🎨 Add logic to button favs and save in localStorage

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -3,17 +3,42 @@ import styles from './list.module.sass'
 import Post from '../post'
 import { initialStateTypes } from '../../actions/list/interfaces'
 import { itemNew } from '../../actions/list/interfaces'
+import useLocalStorage from '../../hooks/useLocalStorage'
 
 interface listInterface {
   state: initialStateTypes
 }
 
 const List = ({ state }: listInterface) => {
+  const [favList, setFavList] = useLocalStorage<string[]>('favs', [])
+
+  const searchId = (id: number) => {
+    return favList.find((i: string) => i === id.toString())
+  }
+
+  const handleFav = (id: number) => {
+    const isFound = searchId(id)
+    let newCurrent
+    if (isFound) {
+      newCurrent = favList.filter((i: string) => i !== id.toString())
+    } else {
+      newCurrent = [...favList, id.toString()]
+    }
+    setFavList(newCurrent)
+  }
   return (
     <div className={styles.wrapper}>
       {state.hits.map((item: itemNew, index: number) => {
         if (item.story_title && item.story_url && item.author && item.created_at) {
-          return <Post key={index} item={item} />
+          return (
+            <Post
+              key={index}
+              item={item}
+              favList={favList}
+              handleFav={handleFav}
+              searchId={searchId}
+            />
+          )
         }
       })}
     </div>
