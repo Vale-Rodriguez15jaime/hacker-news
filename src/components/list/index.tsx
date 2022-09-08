51209302b7f5f6e8c88787bdc6cc06ad30ef97e2
Valeria RodriguezJ <valeria.rodriguez@simetrik.com>
2022-09-07 19:03:30 -0500

    🎨 Update component list to generic component

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -3,40 +3,24 @@ import styles from './list.module.sass'
 import Post from '../post'
 import { initialStateTypes } from '../../actions/list/interfaces'
 import { itemNew } from '../../actions/list/interfaces'
-import useLocalStorage from '../../hooks/useLocalStorage'
 
 interface listInterface {
-  state: initialStateTypes
+  results: itemNew[],
+  favList: string[],
+  setFavList: Function
 }
 
-const List = ({ state }: listInterface) => {
-  const [favList, setFavList] = useLocalStorage<string[]>('favs', [])
-
-  const searchId = (id: number) => {
-    return favList.find((i: string) => i === id.toString())
-  }
-
-  const handleFav = (id: number) => {
-    const isFound = searchId(id)
-    let newCurrent
-    if (isFound) {
-      newCurrent = favList.filter((i: string) => i !== id.toString())
-    } else {
-      newCurrent = [...favList, id.toString()]
-    }
-    setFavList(newCurrent)
-  }
+const List = ({ results, favList, setFavList }: listInterface) => {
   return (
     <div className={styles.wrapper}>
-      {state.hits.map((item: itemNew, index: number) => {
+      {results.map((item: itemNew, index: number) => {
         if (item.story_title && item.story_url && item.author && item.created_at) {
           return (
             <Post
               key={index}
               item={item}
               favList={favList}
-              handleFav={handleFav}
-              searchId={searchId}
+              handleFav={setFavList}
             />
           )
         }
