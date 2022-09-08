d243b7353ae6ab5aaee62eb9d85eb599dcce93f0
Valeria RodriguezJ <valeria.rodriguez@simetrik.com>
2022-09-08 10:46:00 -0500

    🎨 Update function buildFilters

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -2,9 +2,8 @@ import { filtersType } from '../actions/list/interfaces'
 
 export const buildFilters = (filters: filtersType[]) => {
     let filtersUrl = ''
-    if (filters.length) {
+    if (filters && filters.length) {
         filters.forEach((i: filtersType, index: number) => {
-            console.log('index', index)
             filtersUrl += `${index > 0 ? '&' : '?'}${i.key}=${i.value}`
         })
     }
